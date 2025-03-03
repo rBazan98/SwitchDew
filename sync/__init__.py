4a23a86cc4156e5a9eb5623260fed7cafb71a0b4
rBazan <ronaldbazanes@gmail.com>
2025-03-03 22:28:14 +0100

    Create authentication module

--- sync/__init__.py
+++ sync/__init__.py
@@ -1,6 +1,7 @@
 from .config import CONFIG
 from .local_sync import LocalSync
 from .cloud_sync import CloudSync
-from .sync_manager import SwitchDew
+from .sync_manager import SyncManager
+from .auth_manager import AuthManager
 
-__all__ = ["CONFIG", "LocalSync", "CloudSync", "SwitchDew"]
+__all__ = ["CONFIG", "LocalSync", "CloudSync", "SyncManager", "AuthManager"]
--- sync/sync_manager.py
+++ sync/sync_manager.py
@@ -1,21 +1,38 @@
-from sync import LocalSync
-from sync import CloudSync
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 from sync import CONFIG
-from utils import setup_logger
-import os
+from utils import helpers
 
-class SwitchDew:
-    def __init__(self):
-        self.local_sync = LocalSync(CONFIG["LOCAL_PATH"])
-        self.cloud_sync = CloudSync()
-        self.logger = setup_logger(CONFIG["LOG_FILE"])
+class CloudSync:
+    def __init__(self, credentials_path=CONFIG.CREDENTIAL_PATH):
+        """ Inicializa la autenticación con Google Drive usando un archivo JSON """
+        self.auth = GoogleAuth()
+        self.auth.LoadClientConfigFile(credentials_path)  # Carga el JSON de credenciales
+
+        # Intenta cargar credenciales previamente guardadas
+        if not self.auth.LoadCredentialsFile("credentials.json"):
+            self.auth.LocalWebserverAuth()  # Abre el navegador solo si es necesario
+            self.auth.SaveCredentialsFile("credentials.json")  # Guarda las credenciales
+
+        self.drive = GoogleDrive(self.auth)
+
+        # Verificar si la autenticación fue exitosa
+        if self.auth.credentials is None or self.auth.access_token_expired:
+            print("Autentication Failed.")
+        else:
+            print("Autentication completed.")
 
     def sync_files(self):
-        """ Sincroniza los archivos entre local y la nube. """
-        self.local_sync.get_files()        
+        """ Sync files betwen local files and cloud files. """
+        self.local_sync.get_files()
+        self.cloud_sync.get_files()
+ 
+    def get_auth(self):
+        """Devuelve la autenticación para que otros módulos la usen"""
+        return self.auth
 
     def run(self):
         """ Ejecuta la sincronización. """
-        self.logger.info("Iniciando sincronización...")
+        # self.logger.info("Iniciando sincronización...")
         self.sync_files()
-        self.logger.info("Sincronización finalizada.")
+        # self.logger.info("Sincronización finalizada.")
